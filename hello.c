5ea346a55d943d66b7e68de4bec869366ef1ba3d
JoelToof <jtoof2@huskers.unl.edu>
2021-08-26 13:25:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joel Toof 
+ *         jtoof2@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * A simple hello program in C that displays my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joel Toof\n");
+
+  return 0;
+}
